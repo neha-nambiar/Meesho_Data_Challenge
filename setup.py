bd3330bce32f90352557ac214459f2a81e247f89
neha-nambiar <neha.nambiar.1204@gmail.com>
2024-11-26 05:42:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup, find_packages
 
-# Read the contents of your README file
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name='fashion-attribute-prediction',
-    version='0.1.0',
+    name='Meesho_Data_Challenge',
     author='Neha Mahendran Nambiar',
-    description='A comprehensive fashion attribute prediction model',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    description='Kaggle competition code',
     url='https://github.com/neha-nambiar/Meesho_Data_Challenge',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -55,21 +48,8 @@ setup(
         ]
     },
     
-    # Metadata for PyPI
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    
     # Additional project information
-    keywords='fashion-attribute-prediction machine-learning computer-vision',
+    keywords='meesho-data-challenge machine-learning computer-vision',
     
     # Python version requirements
     python_requires='>=3.8,<3.11',
